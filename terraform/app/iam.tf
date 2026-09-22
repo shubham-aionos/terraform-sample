8789771d66e596f87f8504f274b6ab2a3a3c582a
shubham-aionos <shubham.agarwal@aionos.ai>
2026-09-22 13:13:47 +0530

    Grant app runtime access to RDS secret and runtime artifact

--- terraform/app/iam.tf
+++ terraform/app/iam.tf
@@ -24,3 +24,33 @@ resource "aws_iam_instance_profile" "ec2_ssm" {
   name = "three-tier-ec2-ssm-profile"
   role = aws_iam_role.ec2_ssm.name
 }
+
+
+resource "aws_iam_role_policy" "ec2_app_runtime" {
+  name = "three-tier-ec2-app-runtime"
+  role = aws_iam_role.ec2_ssm.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ReadDatabaseSecret"
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Resource = aws_db_instance.postgres.master_user_secret[0].secret_arn
+      },
+      {
+        Sid    = "ReadApplicationRuntimeArtifact"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = "arn:aws:s3:::three-tier-codepipeline-artifacts-${data.aws_caller_identity.current.account_id}/runtime/psycopg-runtime.zip"
+      }
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}
